ba06fc4118b6f09040d121d5e5c5840f4a925f1a
Voocke <timofeevi579@gmail.com>
2025-09-22 21:24:27 +0300

    bases

--- src/Bases/two.kt
+++ src/Bases/two.kt
@@ -1,2 +1,24 @@
 package Bases
 
+fun main() {
+    print("Введите строку: ")
+    var str = readlnOrNull() ?: "" //AASADDSS
+    str = str.toCharArray().sorted().joinToString("")
+
+    var result = ""
+    var count = 0
+    for (i in 0 .. str.length - 1) { //A
+        for (j in 0 .. str.length - 1){ //A
+            if (str[i] in result){
+                break
+            }
+            else if (str[i] == str[j])
+                count++
+        }
+        if (str[i] !in result) {
+            result += "${str[i]} - $count\n"
+            count = 0
+        }
+    }
+    println(result)
+}
