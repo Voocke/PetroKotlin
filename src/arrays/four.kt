db793cf0bd490f5495434eeb3ca58970b40eac19
Voocke <timofeevi579@gmail.com>
2025-09-26 19:00:14 +0300

    arrays four.kt

---
+++ src/arrays/four.kt
@@ -0,0 +1,47 @@
+package arrays
+var firstArr = mutableListOf<Int>()
+var secondArr = mutableListOf<Int>()
+var whichArr = 0
+var count = 0
+fun main() {
+    input()
+
+    var resultArr = emptyArray<Int>()
+
+    val tempSecondArr = secondArr.toMutableList()
+
+    for (i in firstArr.indices) {
+        for (j in tempSecondArr.indices) {
+            if (firstArr[i] == tempSecondArr[j]) {
+                resultArr += firstArr[i]
+                tempSecondArr.removeAt(j)
+                break
+            }
+        }
+    }
+    resultArr = resultArr.sorted().toTypedArray()
+    println("Пересечение: ${resultArr.contentToString()}")
+}
+fun input() {
+    if (count < 2)
+    println("Введите числа для ${if (whichArr == 0) "первого" else "второго"}" +
+            " массива (по 1 за раз, 'q' для прекращения ввода): ")
+    while (true){
+        if (count == 2){
+            return
+        }
+        val input = readln().trim().lowercase()
+
+        if (input == "q"){
+            count++
+            whichArr++
+            input()
+        }
+        else {
+            if (whichArr == 0)
+                firstArr += input.toIntOrNull()?: 0
+            else if (whichArr == 1)
+                secondArr += input.toIntOrNull()?: 0
+        }
+    }
+}
